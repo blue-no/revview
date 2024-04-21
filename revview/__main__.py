17e82e11b3c329756d6644f14edc05fe7233cbe7
Kota Aono <k.pg.aono@gmail.com>
2024-04-21 17:37:12 +0900

    Change settings file location

--- revview/__main__.py
+++ revview/__main__.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 from PyQt5 import QtWidgets
 
@@ -7,6 +8,8 @@ from revview.main.controller import MainWindowController
 from revview.settings.model import Settings
 
 if __name__ == "__main__":
+    fp = Path(_settings_file)
+    fp.parent.mkdir(parents=True, exist_ok=True)
     settings = Settings.initialize(fp=_settings_file)
     app = QtWidgets.QApplication(sys.argv)
     MainWindowController(settings=settings).show_window()
--- revview/_const.py
+++ revview/_const.py
@@ -1,3 +1,9 @@
-_settings_file = "revview_settings.json"
+import os
+
+appdata = os.getenv("APPDATA")
+if appdata is not None:
+    _settings_file = os.path.join(appdata, "RevView", "revview_settings")
+else:
+    _settings_file = os.path.join(".", "revview_settings")
 _max_image_pixels = 1000000000
 _max_image_size = (1080, 1920)

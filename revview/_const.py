56f0cb99c774470ae835e77e30c3abf85cf36131
Kota Aono <k.pg.aono@gmail.com>
2024-04-23 22:20:12 +0900

    Delete constant

--- revview/_const.py
+++ revview/_const.py
@@ -7,11 +7,5 @@ if __appdata is not None:
 else:
     _settings_file = os.path.join(__cwd, "revview_settings")
 
-__userproflie = os.getenv("USERPROFILE")
-if __userproflie is not None:
-    _file_dialog_root = os.path.join(__userproflie, "Desktop")
-else:
-    _file_dialog_root = os.path.join(__cwd)
-
 _max_image_pixels = 1000000000
 _max_image_size = (1080, 1920)
